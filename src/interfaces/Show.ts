eee7fd52e5b9a5517f7eaf655a6b7b27a2584c94
SaebMasarwa <saeb2312@gmail.com>
2025-04-13 14:26:25 +0300

    new file:   src/interfaces/Show.ts

---
+++ src/interfaces/Show.ts
@@ -0,0 +1,13 @@
+export type ShowType = {
+  _id?: string;
+  title: string;
+  plot: string;
+  poster: string;
+  releaseDate: string;
+  genre: string;
+  likes: string[];
+  rating: number;
+  createdAt: string;
+  seasons: number;
+  episodes: number;
+};
